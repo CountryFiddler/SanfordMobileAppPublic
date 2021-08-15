383a638447f89ccc44eff7359ff49c6373a3365a
Ethan Gordash <71423061+CountryFiddler@users.noreply.github.com>
2021-08-15 00:39:19 -0700

    Testing Adding Commit 2

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -22,7 +22,7 @@ const LoginScreen = props => {
       <View>
         <TextInput
           // Text Input Box for the customer's first name
-          placeholder={'Email Address'}
+          placeholder={'Email'}
           value={email}
           // Displays the value that the user is entering into the text input
           // For example, if the typed 'Bob', then 'Bob' is displayed in the
